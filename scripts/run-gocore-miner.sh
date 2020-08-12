8578d301ff8a57afd6102f5289b41ec1c42d1ee9
error2215 <vsifyz20002000!>
2020-08-12 14:17:30 +0300

    node scripts

---
+++ scripts/run-gocore-miner.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Set credentials to download image from private rep
+# MUST BE REMOVED ON RELEASE WHEN REPO WILL BE NOT PRIVATE
+cat ./access_token.txt | docker login https://docker.pkg.github.com/ -u error2215 --password-stdin
+
+# Download image from github
+docker pull docker.pkg.github.com/core-coin/go-core/gocore:v1.0.1-dev
+
+# Docker contaniner name
+containerName="gocore-$1"
+# Gocore account password
+accountPass=$2
+
+# Run container based on downloaded image
+docker run -d --name "$containerName" --net=host docker.pkg.github.com/core-coin/go-core/gocore:v1.0.1-dev --networkid "$1"
+sleep 2
+# Create account and start mining
+docker exec -it "$containerName" gocore --exec "personal.newAccount(\"$accountPass\"); miner.start(1)" attach /testdata/gocore.ipc
+# Show logs of running node
+docker logs -f "$containerName"
---
+++ scripts/run-gocore.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Set credentials to download image from private rep
+# MUST BE REMOVED ON RELEASE WHEN REPO WILL BE NOT PRIVATE
+cat ./access_token.txt | docker login https://docker.pkg.github.com/ -u error2215 --password-stdin
+
+# Download image from github
+docker pull docker.pkg.github.com/core-coin/go-core/gocore:v1.0.1-dev
+
+
+containerName="gocore-$1"
+
+# Run container based on downloaded image
+docker run -d --name "$containerName" --net=host docker.pkg.github.com/core-coin/go-core/gocore:v1.0.1-dev --networkid "$1"
+
+# Show logs of running node
+docker logs -f "$containerName"
