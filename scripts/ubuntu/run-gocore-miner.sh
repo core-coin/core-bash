b8159a472aed31b24e5d33ac89adc3711df83bd7
Rastislav <rastislav@corecoin.cc>
2020-08-31 23:38:45 +0200

    Update files
    Change settings regards to latest release

--- scripts/ubuntu/run-gocore-miner.sh
+++ scripts/ubuntu/run-gocore-miner.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-# Set credentials to download image from private rep
-# MUST BE REMOVED ON RELEASE WHEN REPO WILL BE NOT PRIVATE
-cat ./access_token.txt | docker login https://docker.pkg.github.com/ -u error2215 --password-stdin
+# Check if Network name is provided
+if [ -z "$1" ]
+  then
+    echo "No Network name provided"
+    exit 1
+fi
+# Check if Account password is provided
+if [ -z "$2" ]
+  then
+    echo "No Account password provided"
+    exit 2
+fi
+# Check if Number of threads is provided
+if [ -z "$3" ]
+  then
+    echo "No number of threads provided"
+    exit 3
+fi
 
 # Download image from github
 docker pull docker.pkg.github.com/core-coin/go-core/gocore:latest
@@ -15,9 +30,11 @@ accountPass=$2
 minerThreads=$3
 
 # Run container based on downloaded image
-docker run -d --name "$containerName" --net=host docker.pkg.github.com/core-coin/go-core/gocore:latest --networkid "$1"
+docker run -d --name "$containerName" --net=host docker.pkg.github.com/core-coin/go-core/gocore:latest --$1 --nat=auto
 sleep 2
 # Create account and start mining
-docker exec -it "$containerName" gocore --exec "personal.newAccount(\"$accountPass\"); miner.start($minerThreads)" attach /testdata/gocore.ipc
+docker exec -it "$containerName" gocore --exec "personal.newAccount(\"$accountPass\"); miner.start($minerThreads)" attach ~/.$1/gocore.ipc
 # Show logs of running node
 docker logs -f "$containerName"
+
+exit 0
--- scripts/ubuntu/run-gocore.sh
+++ scripts/ubuntu/run-gocore.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-# Set credentials to download image from private rep
-# MUST BE REMOVED ON RELEASE WHEN REPO WILL BE NOT PRIVATE
-cat ./access_token.txt | docker login https://docker.pkg.github.com/ -u error2215 --password-stdin
+# Check if Network name is provided
+if [ -z "$1" ]
+  then
+    echo "No Network name provided"
+    exit 1
+fi
 
 # Download image from github
 docker pull docker.pkg.github.com/core-coin/go-core/gocore:latest
 
-
 containerName="gocore-$1"
 
 # Run container based on downloaded image
-docker run -d --name "$containerName" --net=host docker.pkg.github.com/core-coin/go-core/gocore:latest --networkid "$1"
+docker run -d --name "$containerName" --net=host docker.pkg.github.com/core-coin/go-core/gocore:latest --$1 --nat=auto
 
 # Show logs of running node
 docker logs -f "$containerName"
+
+exit 0
